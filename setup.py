0a630363e223d5799003ad84738b49d8539deace
Linyuan Gong <gonglinyuan@hotmail.com>
2023-11-15 11:44:06 -0800

    Remove custom build command

--- setup.py
+++ setup.py
@@ -7,50 +7,50 @@ import subprocess
 from setuptools import Command, find_packages, setup
 
 
-class CustomBuildCommand(Command):
-    """Custom command to run build.sh operations."""
-
-    description = 'Run custom build steps'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        # List of commands to run
-        commands = [
-            'git clone https://github.com/tree-sitter/tree-sitter-go',
-            'git clone https://github.com/tree-sitter/tree-sitter-javascript',
-            'git clone https://github.com/tree-sitter/tree-sitter-python',
-            'git clone https://github.com/tree-sitter/tree-sitter-ruby',
-            'git clone https://github.com/tree-sitter/tree-sitter-php',
-            'git clone https://github.com/tree-sitter/tree-sitter-java',
-            'git clone https://github.com/tree-sitter/tree-sitter-c-sharp'
-        ]
-
-        for cmd in commands:
-            subprocess.check_call(cmd, shell=True)
-
-        from tree_sitter import Language
-
-        Language.build_library(
-            # Store the library in the `build` directory
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'code_bleu', 'parser', 'my-languages.so'),
-
-            # Include one or more languages
-            [
-                'tree-sitter-go',
-                'tree-sitter-javascript',
-                'tree-sitter-python',
-                'tree-sitter-php',
-                'tree-sitter-java',
-                'tree-sitter-ruby',
-                'tree-sitter-c-sharp',
-            ]
-        )
+# class CustomBuildCommand(Command):
+#     """Custom command to run build.sh operations."""
+#
+#     description = 'Run custom build steps'
+#     user_options = []
+#
+#     def initialize_options(self):
+#         pass
+#
+#     def finalize_options(self):
+#         pass
+#
+#     def run(self):
+#         # List of commands to run
+#         commands = [
+#             'git clone https://github.com/tree-sitter/tree-sitter-go',
+#             'git clone https://github.com/tree-sitter/tree-sitter-javascript',
+#             'git clone https://github.com/tree-sitter/tree-sitter-python',
+#             'git clone https://github.com/tree-sitter/tree-sitter-ruby',
+#             'git clone https://github.com/tree-sitter/tree-sitter-php',
+#             'git clone https://github.com/tree-sitter/tree-sitter-java',
+#             'git clone https://github.com/tree-sitter/tree-sitter-c-sharp'
+#         ]
+#
+#         for cmd in commands:
+#             subprocess.check_call(cmd, shell=True)
+#
+#         from tree_sitter import Language
+#
+#         Language.build_library(
+#             # Store the library in the `build` directory
+#             os.path.join(os.path.dirname(os.path.abspath(__file__)), 'code_bleu', 'parser', 'my-languages.so'),
+#
+#             # Include one or more languages
+#             [
+#                 'tree-sitter-go',
+#                 'tree-sitter-javascript',
+#                 'tree-sitter-python',
+#                 'tree-sitter-php',
+#                 'tree-sitter-java',
+#                 'tree-sitter-ruby',
+#                 'tree-sitter-c-sharp',
+#             ]
+#         )
 
 
 setup(
@@ -73,7 +73,7 @@ setup(
         'Topic :: Software Development :: Compilers',
         'Topic :: Text Processing :: Linguistic',
     ],
-    cmdclass={
-        'build': CustomBuildCommand,
-    }
+    # cmdclass={
+    #     'build': CustomBuildCommand,
+    # }
 )
